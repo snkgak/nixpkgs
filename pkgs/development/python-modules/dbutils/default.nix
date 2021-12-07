b97660f28f0a6b1a0de062596dadfb30541e5a01
Jonathan Ringer <jonringer117@gmail.com>
2021-12-07 00:56:03 -0800

    python3Packages.dbutils: 2.0.2 -> 2.0.3

--- pkgs/development/python-modules/dbutils/default.nix
+++ pkgs/development/python-modules/dbutils/default.nix
@@ -5,13 +5,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "2.0.2";
+  version = "2.0.3";
   pname = "dbutils";
 
   src = fetchPypi {
     inherit version;
     pname = "DBUtils";
-    sha256 = "1cc8zyd4lapzf9ny6c2jf1vysphlhr19m8miyvw5spbyq4pxpnsf";
+    sha256 = "7d4fa60e9d9e140144566107da387b7c07ba62908b48eefff05ef3afac3202d1";
   };
 
   checkInputs = [ pytestCheckHook ];
